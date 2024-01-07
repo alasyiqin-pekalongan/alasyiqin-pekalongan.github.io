614b8087706930af37b875ab9e6c4a2263df86d1
alasyiqin-pekalongan <alasyiqinpekalongan@gmail.com>
2024-01-07 17:04:01 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+document.getElementById('learnMoreBtn').addEventListener('click', function() {
+    // Ganti URL dengan URL halaman "About" Anda
+    window.location.href = '/index.html';
+});
+
+// function searchContent() {
+//             // Ambil nilai pencarian
+//             var searchTerm = document.getElementById('search').value.toLowerCase();
+
+//             // Cek apakah pencarian adalah "faya ayyuharroju"
+//             if (searchTerm === 'faya ayyuharroju') {
+//                 // Tampilkan konten yang dicari
+//                 document.getElementById('faya_ayyuharroju').classList.add('show');
+//             } else {
+//                 // Sembunyikan konten jika pencarian tidak cocok
+//                 document.getElementById('faya_ayyuharroju').classList.remove('show');
+//             }
+//         }
