fcee2d5d21dc27cc6f845e2c71b6d7840341e160
alasyiqin-pekalongan <alasyiqinpekalongan@gmail.com>
2024-03-02 18:40:10 +0700

    memperbarui sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,28 @@
-const cacheName = "alasyiqin-pekalongan"
-const preCache = ["/","/css","/js","/img","/fitur"] //backup
+// const cacheName = "alasyiqin-pekalongan"
+// const preCache = ["/","/css","/js","/img","/fitur"] //backup
 
-self.addEventListener("install", (e)=>{
-  console.log("service worker installed")
-  e.waitUntil ((async ()=>{
-    const cache = await caches.open(cacheName)
-    cache.addAll(preCache) //(isi preCache)
-  })())
-})
+// self.addEventListener("install", (e)=>{
+//   console.log("service worker installed")
+//   e.waitUntil ((async ()=>{
+//     const cache = await caches.open(cacheName)
+//     cache.addAll(preCache) //(isi preCache)
+//   })())
+// })
 
-self.addEventListener("fetch",(e)=>{
-  e.respondWith((async ()=>{
-    const cache = await caches.open(cacheName)
-    const resCache = await cache.match(e.request)
+// self.addEventListener("fetch",(e)=>{
+//   e.respondWith((async ()=>{
+//     const cache = await caches.open(cacheName)
+//     const resCache = await cache.match(e.request)
     
-    if(resCache) return resCache
+//     if(resCache) return resCache
     
-    try {
-      const res = await fetch(e.request)
+//     try {
+//       const res = await fetch(e.request)
       
-      cache.put(e.request,res.clone())
-      return res
-    } catch (error){
-      console.log(error)
-    }
-  })())
-})
+//       cache.put(e.request,res.clone())
+//       return res
+//     } catch (error){
+//       console.log(error)
+//     }
+//   })())
+// })
